3c8ba81561fe7f3d48d33721f776f645dba19b9f
Bragadeesh J <bjegannathan@rpxcorp.com>
2015-11-24 14:51:45 +0530

    retain these alone

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   searchable do
     text :name, stored: true
-    text :body, stored: true
+    text :body, stored: true, more_like_this: true
 
     integer :id, stored: true
     time    :published_at
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+require "sunspot/rails/solr_logging"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
